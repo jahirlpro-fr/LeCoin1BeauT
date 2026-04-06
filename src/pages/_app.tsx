39b4327582c29a077425ef4f575ac6ac215320f6
softgenai <admin@softgen.ai>
2026-04-06 21:56:37 +0000

    Edit /src/pages/_app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,13 @@
 import { Toaster } from "@/components/ui/toaster";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { CartProvider } from "@/contexts/CartContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Component {...pageProps} />
-      <Toaster />
-    </>
-  );
+    return (
+        <CartProvider>
+            <Component {...pageProps} />
+            <Toaster />
+        </CartProvider>
+    );
 }
